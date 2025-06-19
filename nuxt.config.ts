af5dc3653c0f3c83bf3affbb57f4a96025c6f38f
JIANWE <76219115+JV-HUANG@users.noreply.github.com>
2025-06-19 17:11:28 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,76 @@
+import { aliases } from 'vuetify/iconsets/mdi'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'vuetify-nuxt-module',
+    'nuxt-auth-utils',
+    'nuxt-echarts',
+    '@nuxt/icon',
+    '@nuxt/eslint',
+    '@nuxt/test-utils/module',
+  ],
+  css: ['~/assets/styles/index.css'],
+  experimental: { typedPages: true },
+  typescript: { shim: false, strict: true },
+  vue: { propsDestructure: true },
+  vueuse: { ssrHandlers: true },
+  vuetify: {
+    moduleOptions: {
+      ssrClientHints: {
+        viewportSize: true,
+        prefersColorScheme: true,
+        prefersColorSchemeOptions: {},
+        reloadOnFirstRequest: true,
+      },
+    },
+  },
+  icon: {
+    clientBundle: {
+      icons: Object.values(aliases).map((v) =>
+        (v as string).replace(/^mdi-/, 'mdi:'),
+      ),
+      scan: true,
+    },
+    customCollections: [
+      {
+        prefix: 'custom',
+        dir: './app/assets/icons',
+      },
+    ],
+  },
+  echarts: {
+    charts: ['LineChart', 'BarChart', 'PieChart', 'RadarChart'],
+    renderer: 'svg',
+    components: [
+      'DataZoomComponent',
+      'LegendComponent',
+      'TooltipComponent',
+      'ToolboxComponent',
+      'GridComponent',
+      'TitleComponent',
+      'DatasetComponent',
+      'VisualMapComponent',
+    ],
+  },
+  vite: {
+    build: { sourcemap: false },
+  },
+  runtimeConfig: {
+    github: {
+      clientId: '',
+      clientSecret: '',
+    },
+    session: {
+      name: 'nuxt-session',
+      password: '',
+    },
+  },
+  compatibilityDate: '2024-08-05',
+})
